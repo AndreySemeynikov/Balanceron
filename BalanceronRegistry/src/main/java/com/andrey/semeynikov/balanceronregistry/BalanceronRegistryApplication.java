c63a68dd5e8ddd0f2cea2a4f8730be8dff34e632
Andrey Semeynikov <andrey.semeynikov@gmail.com>
2025-04-26 15:37:54 +0300

    delete balanceron registry

--- BalanceronRegistry/src/main/java/com/andrey/semeynikov/balanceronregistry/BalanceronRegistryApplication.java
+++ BalanceronRegistry/src/main/java/com/andrey/semeynikov/balanceronregistry/BalanceronRegistryApplication.java
@@ -8,8 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BalanceronRegistryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BalanceronRegistryApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(BalanceronRegistryApplication.class, args);
+  }
 }
